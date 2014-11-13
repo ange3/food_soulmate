ce6706d87b0c7edfe50f4f48660e26c60f0c7ba5
Angela Sy <angelasy3@gmail.com>
2014-11-12 21:12:37 -0800

    added file coontaining map from userid to nodeid

--- json test code.py
+++ json test code.py
@@ -1,7 +1,7 @@
-import json
+import json, os, pickle
 from pprint import pprint
-import pickle
 
+# Reads data from a JSON file into an array of elements each of which can be accessed like a map using the element's attributes
 def load_data(file, data):
     infile = open(file, 'r')
     data = json.load(infile)
@@ -9,19 +9,37 @@ def load_data(file, data):
     infile.close()
     return data
     # http://stackoverflow.com/questions/11279331/what-does-the-u-symbol-mean-in-front-of-string-values
-    
-user_data = []
-user_data = load_data('reviews_sample_small.json', user_data)
 
+# Loads JSON data
 # to access data: index into user element and then use key
 # print user_data[0]['user_id']
+def loadJSON(fileName):
+  user_data = []
+  user_data = load_data(fileName, user_data)
+  return user_data
 
+# Creates a userListMap which maps from userID -> nodeID
 # userListMap is a table that allows us to easily index into the user_data (nodeID is the index)
-  # userID -> nodeID
-userListMap = {}
-for index, user in enumerate(user_data):
-  userID = user['user_id']
-  userListMap[userID] = index
+def createMap(user_data):
+  userListMap = {}
+  for index, user in enumerate(user_data):
+    userID = user['user_id']
+    userListMap[userID] = index
+  return userListMap
 
-# print len(userListMap)
 
+# MAIN FUNCTION
+# creates a map that is a user list
+def main():
+  userMapFile = "user_list_map_full.p"
+  # if dictionary does not already exist, load the data
+  if not os.path.isfile(userMapFile):
+    user_data = loadJSON('../comma_separated_dataset/yelp_academic_dataset_user_comma.json')
+    userListMap = createMap(user_data)
+    pickle.dump( userListMap, open( "user_list_map_full.p", "wb" ) )  # save user list map to a file
+  else:
+    userListMap = pickle.load( open( userMapFile, "rb" ) )
+  print len(userListMap)
+  
+# RUN
+main()  
