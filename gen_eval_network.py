9c0db461a23c481bf5fc9f52c0dbd2d16d237675
villanuevab <villanue@stanford.edu>
2014-12-03 23:09:12 -0800

    Update gen_eval_network.py
    doesn't yet print the new edge list file from which we generate the snap network. I want to check this before pushing it -- Blanca

--- gen_eval_network.py
+++ gen_eval_network.py
@@ -1,6 +1,3 @@
-# Generates eval network from data file
-# Generates evaluation_network_edgelist.txt
-
 import json
 import snap
 from pprint import pprint
@@ -17,37 +14,36 @@ def load_data(file, data):
     return data
     # http://stackoverflow.com/questions/11279331/what-does-the-u-symbol-mean-in-front-of-string-values
 
-def write_edges_file(user_map, edgesFile):
+def write_edges_file(user_data, user_map, edgesFile):
   friend_list = []
   file = open(edgesFile, "w")
 
-  for i in xrange(len(user_map)):
-    friend_list.append(user_map[i]['friends'])
+  for i in xrange(len(user_data)):
+    friend_list.append(user_data[i]['friends'])
     # print friend_list[i]
     for j in xrange(len(friend_list[i])):
       # print "user_data[",i,"]['user_id']: ",user_data[i]['user_id']
       # print "friend_list[",i,"][",j,"]: ",friend_list[i][j]
-      line = "{0} {1}\n".format(user_map[user_map[i]['user_id']], user_map[friend_list[i][j]]) 
+      line = "{0} {1}\n".format(user_map[user_data[i]['user_id']], user_map[friend_list[i][j]]) 
       file.write(line)
   # print len(user_data)
   # print "nodeID: ",user_map['7Erd8wom0MYbetSjxvoufQ']
   file.close()
 
 def main(): 
-  edgesFile = 'eval_network_edgelist.txt'
-  userMapFile = '../user_list_map_100.p'
-  # user_data = []
-  # user_data = load_data('../comma_separated_dataset/yelp_academic_dataset_user_comma.json', user_data)
-  # print "friends of a single record: ",user_data[50]['friends']
+  edgesFile = 'evaluation_network_edgelist.txt'
+  userMapFile = 'data/user_list_map_100.p'
+  user_data = []
+  user_data = load_data('../yelp/new_user.json', user_data)
+  # print "friends of a single record: ",user_data[0]['friends']
 
   user_map = pickle.load(open(userMapFile,"rb"))
-  # print len(user_map)
 
-  write_edges_file(user_map, edgesFile)
+  # write_edges_file(user_data, user_map, edgesFile)
 
   g = snap.LoadEdgeList(snap.PUNGraph, edgesFile, 0, 1)
-  snap.PrintInfo(g, "Evaluation Network Info", "eval_network_info.txt", False)
-  snap.SaveEdgeList(g, "eval_network.txt")
+  snap.PrintInfo(g, "Evaluation Network Info", "evaluation_network_info.txt", False)
+  # snap.SaveEdgeList(g, "evaluation_network.txt")
 
 if __name__ == '__main__':
   main()
