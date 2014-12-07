3fba2a070e22edd1d7b51fe8e28f02dd75b101d3
Angela Sy <angelasy3@gmail.com>
2014-12-07 09:25:12 -0800

    modified runme file to load food network graphs

--- runme.py
+++ runme.py
@@ -13,23 +13,32 @@ Create eval network to test food networks against
 Calculates accuracy of each food network vs eval network
 '''
 def main():
-  # Generate Random User files and get path names
+  # 1) Generate Random User files and get path names
   genUsers.genRandomUsers(NUM_USERS)
   # userJSONFile = "../yelp/user_{}_random.json".format(NUM_USERS)
   userMapFile = "data/user_list_map_{}_random.p".format(NUM_USERS)
   genUsers.genReviews(userMapFile, NUM_USERS)
   reviewJSONFile = "../yelp/reviews_by_{}_users_random.json".format(NUM_USERS)
 
+  # 2) Food Network
   # Create Food Network text files
   thresholdJaccard = 0
   thresholdRating = 0
   thresholdAttr = 0
   genFoodNet.createFoodNetworkEdgeLists(userMapFile, reviewJSONFile, thresholdJaccard, thresholdRating, thresholdAttr, NUM_USERS)
+  # Load food networks
+  jaccardNtwkFile = 'food_ntwk_random/jacc_edge_list_{}users_{}.txt'.format(NUM_USERS, thresholdJaccard)
+  attrNtwkFile = 'food_ntwk_random/attr_edge_list_{}users_{}.txt'.format(NUM_USERS, thresholdRating)
+  ratiNtwkFile = 'food_ntwk_random/rati_edge_list_{}users_{}.txt'.format(NUM_USERS, thresholdAttr)
+  jaccardGraph = snap.LoadEdgeList(snap.PUNGraph, jaccardNtwkFile, 0, 1)
+  attrGraph = snap.LoadEdgeList(snap.PUNGraph, attrNtwkFile, 0, 1)
+  ratiGraph = snap.LoadEdgeList(snap.PUNGraph, ratiNtwkFile, 0, 1)
 
-  # Load Eval Network (entire graph)
+  # 3) Load Eval Network (entire graph)
   # TO-DO
+  evalNtwkFile = 'eval_ntwk/eval_edge_list_phoenix.txt'
 
-  # Calculate Accuracy
+  # 4) Calculate Accuracy
   # TO-DO
 
 main()
