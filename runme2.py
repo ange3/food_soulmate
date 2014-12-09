03f6f30d2f12a3e08febf95c32f3fd2daf4681d8
Blanca Villanueva <villanue@stanford.edu>
2014-12-09 00:39:34 -0800

    implemented first half of runTrial in runme2.py

--- runme2.py
+++ runme2.py
@@ -1,25 +1,73 @@
-import gen_random_users as genUsers
+# import gen_random_users as genUsers
 import gen_food_network as genFoodNet
 import gen_eval_network as genEvalNet
 import CNMCommunity as CNM
 import gen_eval_clusters_toImport as genEvalClst
 import json, pickle, snap
 import numpy as np
+import random
+import gen_food_network
+import util
 
+
+NUM_THRESHOLD_VALS = 5 # test 3 threshold values
+NUM_USERS = 1000
+NUM_TRIALS = 100
+
+# returns a list of tuples of srcID and dstID of each edge in the ratingEdgeList
 def oneTimeInit():
-	#Placeholder code
-	file = open( _COMMA_SEPARATED_BUSINESSES_JSON_ , "r")
-	businesses = json.load(file)
-	file.close()
+	review_data = util.loadJSON('../yelp/review.json')
+	userMapFile = "data/user_list_map.p"
+	userMap = pickle.load( open( userMapFile, "rb" ) )
+	for userID in userMap.keys():
+		userMap[userID]['restaurantMap'] = {}
+		userMap[userID]['reviewMap'] = {}
+	print 'Generating MetaData...'
+	gen_food_network.createMetaData(review_data, userMap) # this step takes some time (~2 mins)
 
-	# For each business (happens once) (Blanca)
+	ratingEdgeList = []
+	tempEdgeList = []
+	for line in open('eval_ntwk_food_friends/eval_ntwk_edge_list_phoenix2.txt'):
+		tempEdgeList.append(tuple(line.strip().split(' ')))
+
+	for t in tempEdgeList:
+		edge = int(t[0]), int(t[1])
+		ratingEdgeList.append(edge)
+		# attrAverages[edge] = 0.0
+
+	# print ratingEdgeList
+	# return ratingEdgeList, attrAverages
+	return ratingEdgeList, userMap
+
+def runTrial(ratingEdgeList, userMap):	
+	thresholdVals = [0.0] * NUM_THRESHOLD_VALS
+	for i in xrange(NUM_THRESHOLD_VALS):
+		thresholdVals[i] = i * 0.1 # [0.1, 0.2, 0.3, 0.4, 0.5] as test values for threshold
+
+	print 'Selecting 1000 random edges from ratingEdgeList...'
+	randomEdges = []
+	while len(randomEdges) < 1000: # want 1000 random edges 
+		rand_edge = random.choice(ratingEdgeList) # randomly select 1000 edges from ratingEdgeList
+		if rand_edge not in randomEdges:
+			randomEdges.append(rand_edge)
+
+	for thresholdVal in thresholdVals: # for each potential threshold
+		print 'Calculating Attr Comp Scores for thresholdVal: ',thresholdVal,'...'
+		attrVals = []
+		for edge in randomEdges:
+			user1ID = ''
+			user2ID = ''
+			for key in userMap.keys(): # find the correct user IDs from the node ID given
+				if userMap[key]['node_id'] == edge[0]:
+					user1ID = key
+				if userMap[key]['node_id'] == edge[1]:
+					user2ID = key
+			user1 = userMap[user1ID]
+			user2 = userMap[user2ID]
+			attrCompScore = gen_food_network.getAttrCompScore(user1, user2)
+			# print 'Attr Comp Score for ',edge,' is: ',attrCompScore
+			attrVals.append([edge[0], edge[1], attrCompScore])
 	
-		# Calculate rating score between all combinations of 2 authors of that business's reviews
-		# Add rating score to list of [NID, NID, Rating Score], i.e. the training edge list.
-
-	return ratingEdgeList
-
-def runTrial(ratingEdgeList):
 	# Randomly select 1000 edges from ratingEdgeList (Blanca)
 		# For each potential threshold
 			# Calculate attribute score (Blanca)
@@ -31,15 +79,19 @@ def runTrial(ratingEdgeList):
 		# Calculate accuracy of attribute classifications (Larry)
 
 def main():
-	#Initialize business data (happens once) (Blanca)
-	ratingEdgeList = oneTimeInit()
+	# Initialize business data (happens once) (Blanca)
+	print 'running oneTimeInit...'
+	ratingEdgeList, userMap = oneTimeInit()
 
-	for i in xrange(100):
-		runTrial(ratingEdgeList)
-	
+	for i in xrange(NUM_TRIALS):
+		runTrial(ratingEdgeList, userMap)
+
 	# Find potential threshold that yields maximum accuracy (Larry)
 
 	# Generate the (<30255)-complete graph using attribute scores with that threshold (expensive)
 
 	# Compare clusters on the (<30255)-complete graph against true friends.
 		# Recommend any members of node's cluster that isn't a friend.
+
+if __name__ == '__main__':
+  main()
