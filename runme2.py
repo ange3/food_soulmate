dfc6d43a7c3094e9121ee2d6bb65781d3d7776c8
penguinfrk <hrlarry@stanford.edu>
2014-12-09 08:59:10 -0800

    finishing runTrials

--- runme2.py
+++ runme2.py
@@ -10,7 +10,7 @@ import gen_food_network
 import util
 
 
-NUM_THRESHOLD_VALS = 5 # test 3 threshold values
+NUM_THRESHOLD_VALS = 20 # test 3 threshold values
 NUM_USERS = 1000
 NUM_TRIALS = 1
 
@@ -25,6 +25,13 @@ def importJsonUM():
 	um = json.load(file)
 	file.close()
 	return um
+
+def createNodeToUserIDMap(userMap):
+	nodeToUserIDMap = {}
+	for userID in userMap.keys(): 
+		nodeID = userMap[userID]['node_id']
+		nodeToUserIDMap[nodeID] = userID
+	return nodeToUserIDMap
 
 # returns a list of tuples of srcID and dstID of each edge in the ratingEdgeList
 def oneTimeInit():
@@ -106,60 +113,109 @@ def oneTimeInit():
 
 	return ratingEdgeList, userMap
 
-
-def runTrial(ratingEdgeList, userMap):	
+#def runTrial(ratingEdgeList, userMap):
+def runTrial():	
 	thresholdVals = [0.0] * NUM_THRESHOLD_VALS
-	attrEdgeList = {}
 	thresholdData = {}
 
+	review_data = util.loadJSON('../yelp/review.json')
+	userMapFile = "data/user_list_map_phoenix_friendsAndReview.p"
+	userMapToDump = pickle.load( open( userMapFile, "rb" ) )
+	for userID in userMap.keys():
+		userMap[userID]['restaurantMap'] = {}
+		userMap[userID]['reviewMap'] = {}
+
+	# print 'Generating MetaData...'
+	gen_food_network.createMetaData(review_data, userMapToDump) # this step takes some time (~2 mins)
+
+	userMap = importJsonUM()
+	globalBusinessMap = importJsonGBM()
+	numBusinesses = len(globalBusinessMap)
+
+	nodeToUserIDMap = createNodeToUserIDMap(userMap)
+
 	for i in xrange(NUM_THRESHOLD_VALS):
-		thresholdVals[i] = (i + 1) * 0.1 # [0.1, 0.2, 0.3, 0.4, 0.5] as test values for threshold
-		attrEdgeList[thresholdVals[i]] = []
+		thresholdVals[i] = i * 0.02 # [0.1, 0.2, 0.3, 0.4, 0.5] as test values for threshold
 
 	print 'Selecting 1000 random edges from ratingEdgeList...'
-	randomEdges = []
+	sample_size = 10
+	randomEdges = {}
+	for i in xrange(sample_size):
+		businessList = random.sample(globalBusinessMap, 1)
+		business = businessList[0]
+
+		while len(globalBusinessMap[business]) < 2:
+			business = random.sample(globalBusinessMap.keys(), 1)
+		pair = frozenset(random.sample(globalBusinessMap[business], 2))
+		#nodeIDpair = frozenset([ nodeToUserIDMap[pair[0]], nodeToUserIDMap[pair[1]] ])
+		userID1, userID2 = pair
+		ratingScore = gen_food_network.calculateRatingSimForBusiness(userMap[userID1], userMap[userID2], business)
+		attrScore = gen_food_network.getAttrCompScore(userMap[userID1], userMap[userID2])
+		print "Rating score: %f, Attr score: %f" % (ratingScore, attrScore)
+		randomEdges[pair] = {"Rating":ratingScore, "Attribute":attrScore}
+
+	thresholdScore = {}
+	highestAccuracy = 0
+	highestAccuracyThreshold = 0
+
+	for threshold in thresholdVals:
+		correctCount = 0
+		for pair in randomEdges.keys():
+			correct = ((ratingScore > 0) == (attrScore > thresholdVal))
+			if correct:
+				correctCount += 1
+		print "Correctly classified: %d" % correctCount
+		thresholdScore[threshold] = correctCount
+
+
+
+
 	# while len(randomEdges) < 1000: # want 1000 random edges 
 	# 	rand_edge = random.choice(ratingEdgeList) # randomly select 1000 edges from ratingEdgeList
 	# 	if rand_edge not in randomEdges:
 	# 		randomEdges.append(rand_edge)
-	sample_size = 1000
-	randomEdges = random.sample(randomEdgeList, sample_size)
+	
+	#randomEdges = random.sample(randomEdgeList, sample_size)
+	
 
-	for thresholdVal in thresholdVals: # for each potential threshold
-		print 'Calculating Attr Comp Scores for thresholdVal: ',thresholdVal,'...'
-		correct = 0
-		for edge in randomEdges:
-			user1ID = ''
-			user2ID = ''
-			for key in userMap.keys(): # find the correct user IDs from the node ID given
-				if userMap[key]['node_id'] == edge[0]:
-					user1ID = key
-				if userMap[key]['node_id'] == edge[1]:
-					user2ID = key
-			user1 = userMap[user1ID]
-			user2 = userMap[user2ID]
-			attrCompScore = gen_food_network.getAttrCompScore(user1, user2)
-			# print 'Attr Comp Score for ',edge,' is: ',attrCompScore
-			if attrCompScore > thresholdVal: # Draw edge in the Attribute Similarity network
-				attrEdgeList[thresholdVal].append([edge[0], edge[1], attrCompScore])
-				if edge[2] > 0: # Edge exists in Rating Similarity Network
-					correct += 1 # Hit
-			else: # No edge in the Attribute Similarity network
-				if edge[2] <= 0: # No edge in Rating Similarity Network
-					correct += 1 # Correct rejection
-		thresholdData[thresholdVal] = {"Correct":correct, "Accuracy":correct/1000}
 
-	return attrEdgeList, thresholdData
+
+	# for thresholdVal in thresholdVals: # for each potential threshold
+	# 	print 'Calculating Attr Comp Scores for thresholdVal: ',thresholdVal,'...'
+	# 	correct = 0
+	# 	for edge in randomEdges:
+	# 		user1ID = ''
+	# 		user2ID = ''
+	# 		for key in userMap.keys(): # find the correct user IDs from the node ID given
+	# 			if userMap[key]['node_id'] == edge[0]:
+	# 				user1ID = key
+	# 			if userMap[key]['node_id'] == edge[1]:
+	# 				user2ID = key
+	# 		user1 = userMap[user1ID]
+	# 		user2 = userMap[user2ID]
+	# 		attrCompScore = gen_food_network.getAttrCompScore(user1, user2)
+	# 		# print 'Attr Comp Score for ',edge,' is: ',attrCompScore
+	# 		if attrCompScore > thresholdVal: # Draw edge in the Attribute Similarity network
+	# 			attrEdgeList[thresholdVal].append([edge[0], edge[1], attrCompScore])
+	# 			if edge[2] > 0: # Edge exists in Rating Similarity Network
+	# 				correct += 1 # Hit
+	# 		else: # No edge in the Attribute Similarity network
+	# 			if edge[2] <= 0: # No edge in Rating Similarity Network
+	# 				correct += 1 # Correct rejection
+	# 	thresholdData[thresholdVal] = {"Correct":correct, "Accuracy":float(correct)/1000}
+
+	# return attrEdgeList, thresholdData
 
 def main():
 	# Initialize business data (happens once) (Blanca)
 
 	##################################COMMENT THIS OUT AFTER RUNNING IT ONE TIME#########################################
-	print 'running oneTimeInit...'
-	ratingEdgeList, userMap = oneTimeInit()
-
-	for i in xrange(NUM_TRIALS):
-		runTrial(ratingEdgeList, userMap)
+	#print 'running oneTimeInit...'
+	#ratingEdgeList, userMap = oneTimeInit()
+	
+	#for i in xrange(NUM_TRIALS):
+		#runTrial(ratingEdgeList, userMap)
+	runTrial()
 
 	# Find potential threshold that yields maximum accuracy (Larry)
 
